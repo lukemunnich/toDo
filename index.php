6146086779f70d1212fb8f8248f8b11a1188d361
luke munnich <lukemunnich@gmail.com>
2019-08-28 13:17:22 +0200

    linked js and css

--- index.php
+++ index.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel = "stylesheet"
+    type = "text/css"
+    href = "css/Style.css" />
+
+   <script src="js/functions.js"></script> 
+
     <title>to do app</title>
 </head>
 <body>
     
+
+
+    <div class="container">
+		<h2>Simple To Do List</h2>
+    <p><em>Click and drag to reorder, double click to cross an item off.</em></p>
+       
+		<form name="toDoList">
+			<input type="text" name="ListItem"/>
+		</form>
+    
+		<div id="button">Add</div>
+		<br/>
+		<ol></ol>
+      
+      
+    
+    </div>
+      
+    
+    </div>
+
 </body>
 </html>
---
+++ js/functions.js
@@ -0,0 +1,26 @@
+$(document).ready(
+    function(){
+        $('#button').click(
+            function(){
+                var toAdd = $('input[name=ListItem]').val();
+                 $('ol').append('<li>' + toAdd + '</li>');
+            });
+       
+       $("input[name=ListItem]").keyup(function(event){
+          if(event.keyCode == 13){
+            $("#button").click();
+          }         
+      });
+      
+      $(document).on('click','li', function(){
+        $(this).toggleClass('strike');    
+      });
+      
+      $('input').focus(function() {
+        $(this).val('');
+      });
+      
+      $('ol').sortable();  
+      
+    }
+);
