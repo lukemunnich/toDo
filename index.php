34527c353891fa63b7863070072f31905fad25fb
luke munnich <lukemunnich@gmail.com>
2019-08-29 15:04:36 +0200

    added more php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
-
+<?php
 session_start();
-
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,19 +18,38 @@ session_start();
 
     <title>to do app</title>
 
+
 <div class="container">
-		<h2>Simple To Do List</h2>
-        <p><em>Click item to mark as done, double click to delete</em></p>
-       
-		<form name="toDoList">
-			<input type="text" name="ListItem"/>
-		</form>
-    
-		<div id="button">Add</div>
-		<br/>
+<h2>Simple To Do List</h2>
+    <p><em>Click and drag to reorder, double click to delete an item.</em></p>     
+<form role="form" action="index.php" method="post">
+    <input type="text" name="todoEntry">
+    <button type="submit">add</button><br>
+    <br/>
 		<ol></ol>
-
-
+    <?php
+if(isset($_POST["todoEntry"])){
+    // $item = $_SESSION["listItems"];
+    if(!(isset($_SESSION["listItems"]))){
+        $_SESSION["listItems"] = $item;
+        $_SESSION['listItems'] [] = $_POST['todoEntry'];
+        displayist();
+        // var_dump($_SESSION['listItems']);
+    }else{
+        $_SESSION["listItems"] [] = $_POST['todoEntry'];
+        // var_dump($_SESSION['listItems']);
+       displaylist();
+    }
+}
+    // echo "<br>"."\n<ul>\n<li>".$_POST['todoEntry']."</li>\n</ul>";
+function displaylist(){
+    echo "<ul>";
+        foreach($_SESSION['listItems'] as $item){
+            echo "<li>" . $item . "</li>";
+        }
+    echo "<ol>";
+}
+?>
 </div>  
 </div>
 
