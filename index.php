0cce239a421c0d583c85053dd4588838c4c38920
luke munnich <lukemunnich@gmail.com>
2019-09-02 13:33:40 +0200

    changed jquery and added css

--- index.php
+++ index.php
@@ -1,58 +1,65 @@
+<html>
+    <head>
+        <title>AngelPrime to-do List</title>
 
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="lib/Bootstrap/bootstrap.min.css" />
+        <link rel="stylesheet" href="lib/jQuery/jquery.ui.min.css" />
+        <link rel="stylesheet" href="css/todo.css" />
+    </head>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link rel = "stylesheet"
-    type = "text/css"
-    href = "css/Style.css" />
+    <body class="well">
+        <div id="container">
 
-   <script src="js/functions.js"></script> 
+            <div id="header"> To Do List </div>
 
-    <title>to do app</title>
+            <div class="task-list task-container" id="pending">
+                <h3>Pending</h3>
+                <!--<div class="todo-task">
+                    <div class="task-header">Sample Header</div>
+                    <div class="task-date">25/06/1992</div>
+                    <div class="task-description">Lorem Ipsum Dolor Sit Amet</div>
+                </div>-->
+            </div>
 
+            <div class="task-list task-container" id="inProgress">
+                <h3>In Progress</h3>
+            </div>
 
-<div class="container">
-<h2>Simple To Do List</h2>
-    <p><em>Click and drag to reorder, double click to delete an item.</em></p>     
-<form role="form" action="index.php" method="post">
-    <input type="text" name="todoEntry">
-    <button type="submit">add</button><br>
-    <br/>
-		<ol></ol>
-    <?php
-if(isset($_POST["todoEntry"])){
-    // $item = $_SESSION["listItems"];
-    if(!(isset($_SESSION["listItems"]))){
-        $_SESSION["listItems"] = $item;
-        $_SESSION['listItems'] [] = $_POST['todoEntry'];
-        displayist();
-        // var_dump($_SESSION['listItems']);
-    }else{
-        $_SESSION["listItems"] [] = $_POST['todoEntry'];
-        // var_dump($_SESSION['listItems']);
-       displaylist();
-    }
-}
-    // echo "<br>"."\n<ul>\n<li>".$_POST['todoEntry']."</li>\n</ul>";
-function displaylist(){
-    echo "<ul>";
-        foreach($_SESSION['listItems'] as $item){
-            echo "<li>" . $item . "</li>";
-        }
-    echo "<ol>";
-}
-?>
-</div>  
-</div>
+            <div class="task-list task-container" id="completed">
+                <h3>Completed</h3>
+            </div>
 
+            <div class="task-list">
+                <h3>Add a task</h3>
+                <form id="todo-form">
+                    <input type="text" placeholder="Title" />
+                    <textarea placeholder="Description"></textarea>
+                    <input type="text" id="datepicker" placeholder="Due Date (dd/mm/yyyy)" />
+                    <input type="button" class="btn btn-primary" value="Add Task" onclick="todo.add();" />
+                </form>
 
-</body>
+                <input type="button" class="btn btn-primary" value="Clear Data" onclick="todo.clear();" />
+
+                <div id="delete-div">
+                    Drag Here to Delete
+                </div>
+            </div>
+
+            <div style="clear:both;"></div>
+	          <script type="text/javascript" src="lib/jQuery/jquery.min.js"></script>
+            <script type="text/javascript" src="lib/Bootstrap/bootstrap.min.js"></script>
+            <script type="text/javascript" src="lib/jQuery/jquery.ui.min.js"></script>
+            <script type="text/javascript" src="js/todo.js"></script>
+
+            <script type="text/javascript">
+                $( "#datepicker" ).datepicker();
+                $( "#datepicker" ).datepicker("option", "dateFormat", "dd/mm/yy");
+
+                $(".task-container").droppable();
+                $(".todo-task").draggable({ revert: "valid", revertDuration:200 });
+                todo.init();
+            </script>
+
+        </div>
+    </body>
 </html>
