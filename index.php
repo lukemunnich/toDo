cda36127c2f0aa7100f62471563d135ec105c3b4
luke munnich <lukemunnich@gmail.com>
2019-09-02 15:05:22 +0200

    added css

--- index.php
+++ index.php
@@ -1,88 +1,91 @@
 
 <?php
- session_start();
- // Getting Data For Actions In ToDo
- if(isset($_GET['to'])){
-     $key = $_GET['to'];
-     if($_GET['action'] == 'done'){
-        $_SESSION['todo'][$key]['done']=true;
-     }else if($_GET['action'] == 'cancel'){
-       $_SESSION['todo'][$key]['done']=false;
-     }else{
-         unset( $_SESSION['todo'][$key]);
-     }
- }
+session_start();
 ?>
-
 <html>
-    <head>
-        <title>lukeMunnich to-do List</title>
+<head>
+<title>luke Munnich to-do List</title>
 
 <!-- linking jquery and boostrap and caa -->
-        <link rel="stylesheet" href="lib/Bootstrap/bootstrap.min.css" />
-        <link rel="stylesheet" href="lib/jQuery/jquery.ui.min.css" />
-        <link rel="stylesheet" href="css/todo.css" />
-    </head>
+<link rel="stylesheet" href="lib/Bootstrap/bootstrap.min.css" />
+<link rel="stylesheet" href="lib/jQuery/jquery.ui.min.css" />
+<link rel="stylesheet" href="css/todo.css" />
+</head>
+
+<?php
+      
+ // Getting Data For Actions In ToDo
+      if(isset($_GET['to'])){
+      $key = $_GET['to'];
+      if($_GET['action'] == 'done'){
+         $_SESSION['todo'][$key]['done']=true;
+      }else if($_GET['action'] == 'cancel'){
+       $_SESSION['todo'][$key]['done']=false;
+      }else{
+         unset( $_SESSION['todo'][$key]);
+      }
+    }
+
+?>
 
 <!--creating class and id for styling  -->
 
     <body class="well">
         <div id="container">
 
-            <div id="header"> To Do List </div>
+        <div id="header"> To Do app </div>
 
-            <div class="task-list task-container" id="pending">
+        <div class="task-list task-container" id="pending">
                 <h3>Pending</h3>
               
-            </div>
+        </div>
 
-            <div class="task-list task-container" id="inProgress">
+        <div class="task-list task-container" id="inProgress">
                 <h3>In Progress</h3>
-            </div>
+        </div>
 
-            <div class="task-list task-container" id="completed">
+        <div class="task-list task-container" id="completed">
                 <h3>Completed</h3>
-            </div>
+        </div>
 
-<!-- creating form -->
-<!-- Session Start -->
-
-            <div class="task-list">
-                <h3>Add a task</h3>
-                <form method="post" action="index.php" id="todo-form">
-                    <input type="text" placeholder="Title" />
-                    <textarea placeholder="Description"></textarea>
-                    <input type="text" id="datepicker" placeholder="Due Date (dd/mm/yyyy)" />
-                    <input type="button" class="btn btn-primary" value="Add Task" onclick="todo.add();" />
-                </form>
+<!-- !-- creating form --> -->
+        <div class="task-list">
+            <h3>Add a task</h3>
+            <form method="post" action="index.php" id="todo-form">
+                 <input type="text" placeholder="Title" />
+                 <textarea placeholder="Description"></textarea>
+                 <input type="text" id="datepicker" placeholder="Due Date (dd/mm/yyyy)" />
+                 <input type="button" class="btn btn-primary" value="Add Task" onclick="todo.add();" />
+            </form>
 
                 <input type="button" class="btn btn-primary" value="Clear Data" onclick="todo.clear();" />
 
-                <div id="delete-div">
+        <div id="delete-div">
                     Drag Here to Delete
-                </div>
-            </div>
-            <div style="clear:both;"></div>
+        </div>
+        </div>
+        <div style="clear:both;"></div>
 
 
 
          <!-- linking jquery  -->
-	          <script type="text/javascript" src="lib/jQuery/jquery.min.js"></script>
-            <script type="text/javascript" src="lib/Bootstrap/bootstrap.min.js"></script>
-            <script type="text/javascript" src="lib/jQuery/jquery.ui.min.js"></script>
-            <script type="text/javascript" src="js/todo.js"></script>
+<script type="text/javascript" src="lib/jQuery/jquery.min.js"></script>
+<script type="text/javascript" src="lib/Bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="lib/jQuery/jquery.ui.min.js"></script>
+ <script type="text/javascript" src="js/todo.js"></script>
            
 
 <!-- javascript for the date picker and the drag function -->
-            <script type="text/javascript">
+<script type="text/javascript">
                 $( "#datepicker" ).datepicker();
                 $( "#datepicker" ).datepicker("option", "dateFormat", "dd/mm/yy");
 
                 $(".task-container").droppable();
                 $(".todo-task").draggable({ revert: "valid", revertDuration:200 });
                 todo.init();
-            </script>
+</script>
 
-        </div>
-    </body>
+</div>
+
+</body>
 </html>
